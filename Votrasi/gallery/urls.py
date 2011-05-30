356d2485d05bfb4d6dddd1d4a71e66ce1c16b8fb
abhipixelcode <abhilash@pixelcode.ca>
2011-05-30 10:13:11 +0530

    Edited ,Added url for MEDIA

--- Votrasi/gallery/urls.py
+++ Votrasi/gallery/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,7 +10,8 @@ urlpatterns = patterns('',
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-
+    
     (r'^', 'gallery.views.index'),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     
 )
